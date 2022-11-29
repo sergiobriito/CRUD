3a6c996c42e748c44e5256aa973dc229c36957b4
Sérgio Brito <64617586+sergiobriito@users.noreply.github.com>
2022-11-29 12:37:03 -0300

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {useState, useEffect} from "react";
 import {Link, useNavigate} from "react-router-dom";
 import {toast} from "react-toastify";
